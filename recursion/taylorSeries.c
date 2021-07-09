84969f096cb2158b250e1beff58663befbcce85e
tarun kashyap <tarunsinghkashyap@gmail.com>
2021-07-10 04:17:45 +0530

    third commit

--- recursion/taylorSeries.c
+++ recursion/taylorSeries.c
@@ -16,12 +16,36 @@ double e(int x, int n)
         f = f*n;
         return(r + p/f);
     }
+}
+
+double e_horner(int x, int n)
+{
+    static double s = 1;
+    if(n==0)
+        return s;
     
+    s = 1+x*s/n;
+    return e_horner(x, n-1);
+}
+
+double e_iter(int x, int n)
+{
+    double s = 1;
+    int i;
+    double num = 1;
+    double den = 1;
+    for(i=1;i<=n;i++)
+    {
+        num*=x;
+        den*=i;
+        s+=num/den;
+    }
+    return s;
 }
 
 int main()
 {
-    
-    printf("%lf \n", e(4,16));
+   
+    printf("%lf \n", e_iter(2,10));
     return 0;
 }
